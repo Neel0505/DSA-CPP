33993b1b4ba4b51622bb8e7f807d1d473c3a64bc
Neel Parikh <neelparikh0507@gmail.com>
2026-06-29 20:31:19 +0530

    feature: add Remove All Occurrences of a Substring Solution

---
+++ Strings/RemoveAllOccurrences.cpp
@@ -0,0 +1,18 @@
+// Leetcode problem: https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
+// Time Complexity: O(n)
+// Leetcode Problem 1910 Remove All Occurrences of a Substring
+
+/*
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        int pos = s.find(part);
+        while(pos != string::npos){
+            s.erase(pos, part.length());
+            pos = s.find(part);
+        }
+        return s;
+    }
+};
+
+*/
