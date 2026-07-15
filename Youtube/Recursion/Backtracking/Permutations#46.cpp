c86d4111baed117764ac2e06e553734f42c41c49
Neel Parikh <neelparikh0507@gmail.com>
2026-07-15 11:38:50 +0530

    feature: add permutations recursion solution

---
+++ Youtube/Recursion/Backtracking/Permutations#46.cpp
@@ -0,0 +1,27 @@
+// Leetcode Question: 46. Permutations
+// https://leetcode.com/problems/permutations/
+
+/*
+class Solution {
+public:
+    void getAllPermutations(vector<int>& nums, vector<int>& ans, int index){
+        // Base Case
+        if(index == nums.size()){
+            ans.push_back({nums});
+            return;
+        }
+
+        for(int i = 0; i < nums.size(); i++){
+            swap(nums[index], nums[i]);
+            getAllPermutations(nums, ans, index + 1);
+            swap(nums[index], nums[i]); // backtrack
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        getAllPermutations(nums, ans, 0);
+        return ans;
+
+    }
+};
+*/
