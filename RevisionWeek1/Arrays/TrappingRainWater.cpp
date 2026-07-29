92fbad3ff42237db0feeb6fc3f543a7aa1bd8933
Neel Parikh <neelparikh0507@gmail.com>
2026-07-29 09:55:28 +0530

    feat: revision day 1 - trapping rain water

---
+++ RevisionWeek1/Arrays/TrappingRainWater.cpp
@@ -0,0 +1,9 @@
+// Trapping Rain Water Problem
+// Leetcode Problem: https://leetcode.com/problems/trapping-rain-water/
+// Leetcode Problem #42
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+// Reference: DSA-C++\Arrays\container_with_most_water.cpp
+
