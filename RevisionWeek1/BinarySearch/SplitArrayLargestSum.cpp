d2356c361c8cf13643ca231d1b34a0b20e9b0a39
Neel Parikh <neelparikh0507@gmail.com>
2026-07-30 10:36:36 +0530

    feat: revision day 2 - split array largest sum

--- RevisionWeek1/BinarySearch/SplitArrayLargestSum.cpp
+++ RevisionWeek1/BinarySearch/SplitArrayLargestSum.cpp
@@ -8,3 +8,36 @@
 // Reference: DSA-C++\Youtube\BinarySearch\BookAllocationProblem.cpp
 // Reference: DSA-C++\Youtube\BinarySearch\PaintersPartitionProblem.cpp
 
+/*
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int k) {
+        bool isPossible(vector<int> &arr, int n, int m, int maxAllowedSum){ // Time Complexity: O(n)
+            int subarrayCount = 1;
+            int subarraySum = 0;
+
+            for(int i=0; i<n; i++){
+                if(arr[i] > maxAllowedSum) return false; // If a single element is greater than maxAllowedSum, it's not possible to split
+
+                if(subarraySum + arr[i] <= maxAllowedSum){
+                    subarraySum += arr[i];
+                }
+                else{
+                    subarrayCount++;
+                    subarraySum = arr[i];
+                }
+            }
+            return subarrayCount <= m;
+        }
+
+        int n = nums.size();
+        int sum = 0;
+        int maxVal = INT_MIN;
+        for(int i=0; i<n; i++){ // Time Complexity: O(n)
+            sum += nums[i];
+            maxVal = max(maxVal, nums[i]);
+        }
+        
+    }
+};
+*/
